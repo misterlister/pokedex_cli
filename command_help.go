f2d5b44dc66ffdc3a1ebd01f542b9e409a26279b
misterlister <haydenglister@gmail.com>
2024-08-02 11:27:45 -0700

    Added config parameter to commands

--- command_help.go
+++ command_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println(DIVIDER)
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Valid commands:")
