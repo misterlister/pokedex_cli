cd5c7b6dad95549142754347ef134c4eae53f9f7
misterlister <haydenglister@gmail.com>
2024-08-09 11:46:33 -0700

    Added GetLocationArea function

--- internal/pokeapi/request_locations.go
+++ internal/pokeapi/request_locations.go
@@ -56,3 +56,55 @@ func (c *Client) ListLocations(pageURL *string) (LocationsResp, error) {
 
 	return locationAreasResp, nil
 }
+
+func (c *Client) GetLocationArea(area string) (LocationArea, error) {
+	URL := BASEURL + LOCATIONURL + "/" + area
+
+	// check if page is in the cache
+	dat, ok := c.cache.Get(URL)
+
+	if ok {
+		// cache hit
+		locationArea := LocationArea{}
+		err := json.Unmarshal(dat, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+		return locationArea, nil
+	}
+
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 && resp.StatusCode < 500 {
+		return LocationArea{}, fmt.Errorf("error: %s is not a valid area", area)
+	}
+
+	if resp.StatusCode > 499 {
+		return LocationArea{}, fmt.Errorf("error: http status code: %v", resp.StatusCode)
+	}
+
+	dat, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationArea := LocationArea{}
+	err = json.Unmarshal(dat, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	c.cache.Add(URL, dat)
+
+	return locationArea, nil
+}
