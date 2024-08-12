b4ac5ac4bb25e3ab3a8ece048016e924bcd7f073
misterlister <haydenglister@gmail.com>
2024-08-12 13:38:53 -0700

    Implemented location selection by number instead of name

--- command_explore.go
+++ command_explore.go
@@ -3,13 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
-
-	"github.com/misterlister/pokedex_cli/internal/pokeapi"
+	"strconv"
 )
 
 func commandExplore(cfg *config, locationName string) error {
 	if locationName == "" {
-		return errors.New("you must specify a location to explore")
+		if cfg.currentLocation.name == nil {
+			return errors.New("you must specify a location to explore")
+		}
+		locationName = *cfg.currentLocation.name
+	}
+
+	num, err := strconv.Atoi(locationName)
+
+	if err == nil {
+		if num >= len(cfg.currentLocationChoices) {
+			return errors.New("the area number you selected is out of range")
+		}
+		locationName = cfg.currentLocationChoices[num]
 	}
 
 	resp, err := cfg.pokeapiClient.GetLocationArea(locationName)
@@ -18,14 +29,35 @@ func commandExplore(cfg *config, locationName string) error {
 		return err
 	}
 
-	printPokemonInArea(resp, locationName)
+	cfg.currentLocation.data = &resp
+	cfg.currentLocation.name = &locationName
+
+	err = getPokemonInArea(cfg)
+
+	if err != nil {
+		return err
+	}
+
+	printPokemonInArea(cfg)
 
 	return nil
 }
 
-func printPokemonInArea(resp pokeapi.LocationArea, locationName string) {
-	fmt.Printf("Pokemon in %s:\n", locationName)
-	for _, entry := range resp.PokemonEncounters {
-		fmt.Printf("- %s\n", entry.Pokemon.Name)
+func getPokemonInArea(cfg *config) error {
+	if cfg.currentLocation.data == nil {
+		return errors.New("you are not currently in an area")
+	}
+	pokemonList := make([]string, 0)
+	for _, entry := range cfg.currentLocation.data.PokemonEncounters {
+		pokemonList = append(pokemonList, entry.Pokemon.Name)
+	}
+	cfg.currentLocation.localPokemon = pokemonList
+	return nil
+}
+
+func printPokemonInArea(cfg *config) {
+	fmt.Printf("Pokemon in %s:\n", *cfg.currentLocation.name)
+	for i, pokemon := range cfg.currentLocation.localPokemon {
+		fmt.Printf("%d - %s\n", i, pokemon)
 	}
 }
--- command_map.go
+++ command_map.go
@@ -14,7 +14,7 @@ func commandMapf(cfg *config, arg string) error {
 		return err
 	}
 	cfg.locationPage++
-	printMaps(resp, cfg.locationPage)
+	printMaps(resp, cfg)
 	cfg.nextLocationsURL = resp.Next
 	cfg.prevLocationsURL = resp.Previous
 	return nil
@@ -29,16 +29,18 @@ func commandMapb(cfg *config, arg string) error {
 		return err
 	}
 	cfg.locationPage--
-	printMaps(resp, cfg.locationPage)
+	printMaps(resp, cfg)
 	cfg.nextLocationsURL = resp.Next
 	cfg.prevLocationsURL = resp.Previous
 	return nil
 }
 
-func printMaps(resp pokeapi.LocationsResp, page int) {
-	fmt.Printf("Location areas (page %v):\n", page)
-
-	for _, area := range resp.Results {
-		fmt.Printf("- %s\n", area.Name)
+func printMaps(resp pokeapi.LocationsResp, cfg *config) {
+	fmt.Printf("Location areas (page %v):\n", cfg.locationPage)
+	locations := make([]string, 0)
+	for i, area := range resp.Results {
+		fmt.Printf("%d - %s\n", i, area.Name)
+		locations = append(locations, area.Name)
 	}
+	cfg.currentLocationChoices = locations
 }
